428f716db2c5f107fa524d6aa5aaaba544e456b8
gamerdeer3000-web <gamerdeer3000@gmail.com>
2025-08-18 20:36:48 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+// Define a name for the cache
+const CACHE_NAME = 'golf-score-tracker-v1';
+// List the files to cache
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
